993ed840af0124e7b213d1ef6a625a1042bb8e48
Bmorrissey34 <88950762+Bmorrissey34@users.noreply.github.com>
2022-01-13 15:09:59 -0500

    set up schema and seeds

---
+++ db/schema.sql
@@ -0,0 +1,30 @@
+DROP DATABASE IF EXISTS roles_db;
+CREATE DATABASE roles_db;
+
+USE roles_db;
+
+CREATE TABLE department (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    department_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE roles (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary INT NOT NULL,
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+);
+
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL, 
+    manager_id INT,
+    FOREIGN KEY (role_id)
+    REFERENCES roles(id),
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO department (department_name)
+VALUES ('Sales'),
+       ('Engineering'),
+       ('Finance');
+
+INSERT INTO roles (title, salary, department_id)
+VALUES ('Marketing Manager', 100000, 1),
+       ('Sales Lead', 75000, 1),
+       ('Front End Engineer', 150000, 2),
+       ('Software Engineer', 120000, 2),
+       ('Head of Accounting', 170000, 3),
+       ('Accountant', 95000, 3);
