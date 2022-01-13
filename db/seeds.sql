d388439a04c26ab4a9f39b99cba875e273233082
Bmorrissey34 <88950762+Bmorrissey34@users.noreply.github.com>
2022-01-13 15:18:00 -0500

    added seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -10,3 +10,11 @@ VALUES ('Marketing Manager', 100000, 1),
        ('Software Engineer', 120000, 2),
        ('Head of Accounting', 170000, 3),
        ('Accountant', 95000, 3);
+
+INSERT INTO employee (first_name, role_id, manager_id)
+VALUES ('Bob', 'Smith', 1, NULL),
+       ('Joe', 'Howe', 2, 1),
+       ('Jean', 'Smith', 3, NULL),
+       ('Abby', 'Grant', 4, 3),
+       ('Bob', 'Finley', 5, NULL),
+       ('Dick', 'Clark', 6 , 5);
